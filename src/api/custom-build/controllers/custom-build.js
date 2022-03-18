59f668a943b60538cf5a8282c4799e7d88b58702
sis <sis@HERCULES>
2022-03-18 12:07:41 +0300

    update schemas

--- src/api/custom-build/controllers/custom-build.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  custom-build controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::custom-build.custom-build');
--- src/api/custom-build/routes/custom-build.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * custom-build router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::custom-build.custom-build');
